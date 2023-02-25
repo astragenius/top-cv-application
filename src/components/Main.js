23c9bd9c5cb646b88dfcc98d5ab6d92219b5011b
astragenius <darthmalak86@gmail.com>
2023-02-25 18:56:51 +0100

    finshed persoInfo components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import PersonalInput from './PersonalInput'
+import { useState } from 'react'
 
 function Main() {
+
+    const [value, setValue] = useState({})
+
+    const getPersoInfo = (value) => {
+       
+        setValue(value)
+    }
+    
+
   return (
     <div>
-        <PersonalInput/>
+        <PersonalInput getInfo={getPersoInfo}/>
+       
     </div>
   )
 }
--- src/components/PersonalInput.js
+++ src/components/PersonalInput.js
@@ -1,34 +1,52 @@
 import React from 'react'
-import '../styles/personalInput.css'
+import { useState } from 'react'
 
-function PersonalInput() {
+
+function PersonalInput(props) {
+
+    const [name, setName] = useState('');
+    const [lastName, setLastName] = useState('')
+    const [phoneNumber, setPhoneNumber] = useState('')
+    const [email, setEmail] = useState('')
+    const [about, setAbout] = useState('')
+    
+   const sendInfo = props.getInfo;
+
+  const getPersoInfo = (e) => {
+    e.preventDefault()
+    const persoInfo = {name, lastName, phoneNumber, email, about}
+
+    
+    sendInfo(persoInfo)
+  } 
+    
   return (
     <div>
-        <form>
+        <form onSubmit={getPersoInfo}>
             <h3>Personal Details</h3>
             <div className='inputContainer'>
                 <label>First Name</label>
-                <input type="text" name="FirstName"/>
+                <input type="text" name="FirstName" onChange={(e) => setName(e.target.value)}/>
             </div>
             <div className='inputContainer'>
                 <label>Last Name</label>
-                <input type="text" name="LastName" id="" />
+                <input type="text" name="LastName" onChange={(e) => setLastName(e.target.value)}/>
             </div>
             <div className='inputContainer'>
                 <label>Phone Number</label>
-                <input type="tel" name="PnoneNumber" id="" />
+                <input type="tel" name="PnoneNumber" onChange={(e) => setPhoneNumber(e.target.value)}/>
             </div>
             <div className='inputContainer'>
                 <label>Email</label>
-                <input type="email" name="Email" id="" />
+                <input type="email" name="Email" onChange={(e) => setEmail(e.target.value)}/>
             </div>
             <div className='inputContainer'>
 
                 <label htmlFor="">About Me:</label>
-                <textarea name="AboutMe"></textarea>
+                <textarea name="AboutMe" onChange={(e) => setAbout(e.target.value)}></textarea>
             </div>
 
-            
+            <button type='submit'>Submit</button>
         </form>
     </div>
   )
